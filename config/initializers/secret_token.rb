b2c867f2258f3226581d9f0948c820a7d391c28a
timakin <timaki.st@gmail.com>
2014-10-01 00:14:06 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pion::Application.config.secret_key_base = 'b1495a214f7ce0641582cfd59750f6d28f4eb2d40f5567ded97e488c79b39e36b11b20c480fbdf72279a031a5450d5af0580bc715a7d9553b568a147be564adc'
